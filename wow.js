7aa2e7a88c4f64697b803bc2e759faed431bfcc9
GEO-FS-GUY <zach09700@gmail.com>
2026-03-08 19:33:57 +1100

    Create wow.js

---
+++ wow.js
@@ -0,0 +1,8 @@
+<script>
+  // /Just-some-stall-sound/
+  var audio = new Audio('C:\Users\mzach\Downloads\fast_alarm_for_morning.mp3');
+  function playAudio() {
+    audio.play();
+  }
+</script>
+<button onclick="playAudio()">Play Sound</button>
